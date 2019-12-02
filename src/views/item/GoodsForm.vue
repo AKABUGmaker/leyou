4bd03832d93f1d2c3638af85bead075761a17a7c
xiaoFan <775069279@qq.com>
2019-12-02 20:12:10 +0800

    乐优前端(第七天完成)

--- src/views/item/GoodsForm.vue
+++ src/views/item/GoodsForm.vue
@@ -5,9 +5,9 @@
       <v-divider/>
       <v-stepper-step :complete="step > 2" step="2">商品描述</v-stepper-step>
       <v-divider/>
-      <v-stepper-step :complete="step > 3" step="3">规格参数</v-stepper-step>
+      <v-stepper-step :complete="step > 3" step="3">通用规格参数</v-stepper-step>
       <v-divider/>
-      <v-stepper-step step="4">SKU属性</v-stepper-step>
+      <v-stepper-step step="4">特有SKU属性</v-stepper-step>
     </v-stepper-header>
     <v-stepper-items>
       <!--1、基本信息-->
